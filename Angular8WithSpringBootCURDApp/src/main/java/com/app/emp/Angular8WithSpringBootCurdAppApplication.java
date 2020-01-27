68001cb60dace9da7748cb87621afd669504e90c
Ashutosh Kumar <ashuptn92@outlook.com>
2020-01-27 22:16:39 +0530

    Spring java code first commit by Ashutosh

---
+++ Angular8WithSpringBootCURDApp/src/main/java/com/app/emp/Angular8WithSpringBootCurdAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.emp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Angular8WithSpringBootCurdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Angular8WithSpringBootCurdAppApplication.class, args);
+	}
+
+}
